9b83a12f85c093ed1423870157b7bc7bfd01624a
dragonballzeke <zeke@egherman.com>
2019-10-30 16:02:47 -0500

    Only calculate dates if event live

--- event.py
+++ event.py
@@ -3,12 +3,14 @@ import datetime,pytz
 
 class Event:
     def __init__(self,data):
+        self.current_event = None
         for key in data:
             setattr(self,key,data[key])
         self.data = data
-        self.start = datetime.datetime.fromtimestamp(data['current_event']['starts_at']).astimezone(pytz.timezone(self.timezone))
+        if self.current_event:
+            self.start = datetime.datetime.fromtimestamp(data['current_event']['starts_at']).astimezone(pytz.timezone(self.timezone))
+            self.month = self.start.strftime('%B')
+            self.short_month = self.start.strftime('%b')
+            self.day = self.start.day
+            self.year = self.start.strftime('%Y')
         self.url = self.id
-        self.month = self.start.strftime('%B')
-        self.short_month = self.start.strftime('%b')
-        self.day = self.start.day
-        self.year = self.start.strftime('%Y')
