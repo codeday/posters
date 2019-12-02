cc208a198c6468a3f0256de70c520e20dc39518f
dragonballzeke <zeke@egherman.com>
2019-12-02 10:06:52 -0600

    Api POC - it works!

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,send_file
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from event import Event
+import requests, json
+
+app = Flask('posters')
+env = Environment(
+    loader= FileSystemLoader('./templates'),
+    autoescape=select_autoescape(['svg'])
+)
+print(env.list_templates())
+@app.route('/')
+def index():
+    return "Poster generator"
+
+
+@app.route('/generate/<id>/<template>/<file_format>')
+def generate(id, template, file_format='svg'):
+    event = requests.get('https://clear.codeday.org/api/region/{}'.format(id))
+    if event.status_code is 200:
+        event = Event(json.loads(event.text))
+    return send_file(event.make_poster(template,file_format))
--- event.py
+++ event.py
@@ -1,4 +1,11 @@
-import datetime,pytz
+import datetime, pytz, os
+from make_pdf import make_pdf
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader= FileSystemLoader('./templates'),
+    autoescape=select_autoescape(['svg'])
+)
 
 
 class Event:
@@ -14,3 +21,39 @@ class Event:
             self.day = self.start.day
             self.year = self.start.strftime('%Y')
         self.url = self.id
+
+    def make_poster(self,template, file_format):
+        template += '.svg'
+        supported_files = ['svg','pdf']
+        file_format = file_format.lower()
+        if file_format not in supported_files:
+            print('ERROR: File provided not supported format')
+            raise
+        if template not in env.list_templates():
+            print('ERROR: Template provided not loaded in environment')
+            raise
+        if not self.current_event:
+            print('ERROR: Event not live in Clear')
+            raise
+        file = template
+        template = env.get_template(file)
+        id = self.id
+        if not os.path.exists('generated/'):
+            os.mkdir('generated/')
+        if not os.path.exists('generated/{}/'.format(id)):
+            os.mkdir('generated/{}/'.format(id))
+        if not os.path.exists('generated/{}/svg/'.format(id)):
+            os.mkdir('generated/{}/svg/'.format(id))
+        if not os.path.exists('generated/{}/pdf/'.format(id)):
+            os.mkdir('generated/{}/pdf/'.format(id))
+        with open("generated/{}/svg/{}".format(id, file), "w+") as f:
+            f.write(template.render(**vars(self)))
+        if file_format == 'pdf':
+            make_pdf('generated/{}/svg/{}'.format(id, file),
+                     'generated/{}/pdf/{}'.format(id, file.replace('.svg', '.pdf')))
+            return 'generated/{}/pdf/{}'.format(id, file.replace('.svg', '.pdf'))
+        return 'generated/{}/svg/{}'.format(id, file)
+
+    def make_posters(self,templates=env.list_templates()):
+        for template in templates:
+            self.make_poster(template)
