b4b609f7e0447d7a01ba4c32dccb460f6d56c08b
Nikolas Huebecker <nikolas.huebecker@gmail.com>
2019-12-09 18:32:14 -0500

    Refactor with React Hooks

--- posters/src/poster.js
+++ posters/src/poster.js
@@ -3,9 +3,17 @@ import Loading from './loading.js'
 
 
 const Poster = (props) => {
-    return(
-        <img src={`localhost:5000/generate/${props.posterRegion}/${props.posterTemplate}/${props.posterFormat}`}/>
-    )
+    const regionNames = props.regions.map(region => region.name)
+
+    if (regionNames.includes(props.posterRegion)) {
+        return(
+            <img src={`localhost:5000/generate/${props.posterRegion}/${props.posterTemplate}/${props.posterFormat}`}/>
+        )
+    } else {
+        return(
+            <p>Please use a Region from the List</p>
+        )
+    }
 }
 
 export default Poster
--- posters/src/posters.js
+++ posters/src/posters.js
@@ -1,15 +1,39 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import EventDropdown from './eventDropdown.js';
 import TemplateDropdown from './templateDropdown.js';
 import Poster from './poster.js';
 const Posters = () => {
-    const [region, setRegion] = useState();
+    const [regions, setRegions] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    const [posterRegion, setPosterRegion] = useState('');
+    const [posterTemplate, setPosterTemplate] = useState("Blobs");
+    const [posterFormat, setPosterFormat] = useState("svg");
+
+    useEffect(() => {
+        if (regions.length == 0) {
+            fetch("https://clear.codeday.org/api/regions/")
+                .then(res => res.json())
+            .then(
+                (result) => {
+                    setLoading(true)
+                    setRegions(result.filter(region => region.current_event != null))
+                },
+                (error) => {
+                    setLoading(true)
+                    setError(error)
+                }
+            )
+        }
+    })
+
     return(
-            <>
+        <>
             <h1>CodeDay Poster Generator</h1>
-                <EventDropdown setRegion={setRegion} />
-                <Poster posterRegion={region} posterTemplate={"Blobs"} posterFormat={"svg"}/>
-                </>
-        )
+            <EventDropdown error={error} isLoading={loading} regions={regions} setRegion={setPosterRegion} />
+            <Poster regions={regions} posterRegion={posterRegion} posterTemplate={posterTemplate} posterFormat={posterFormat}/>
+        </>
+    )
 }
 export default Posters
